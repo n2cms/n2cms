ba0b76538ebc6a693f58814534200d18b21c12ab
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2008-03-22 02:00:14 +0000

    - removed some old license comments

--- src/Core/N2/Utility.cs
+++ src/Core/N2/Utility.cs
@@ -1,15 +1,3 @@
-#region License
-
-/* Copyright (C) 2006-2007 Cristian Libardo
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- */
-
-#endregion
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- src/Core/N2/Web/UI/WebControls/EditableDisplay.cs
+++ src/Core/N2/Web/UI/WebControls/EditableDisplay.cs
@@ -1,15 +1,3 @@
-#region License
-
-/* Copyright (C) 2007 Cristian Libardo
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- */
-
-#endregion
-
 using System;
 using System.Web.UI;
 using N2.Definitions;
