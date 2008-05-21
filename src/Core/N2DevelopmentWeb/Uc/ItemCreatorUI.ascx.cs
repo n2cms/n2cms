648222de4a11354ff0a77ad0c362f1f09c01ab5b
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2008-05-21 15:05:52 +0000

    - removed some obsolte code: CloseSessionModule, IDataItemContainer, NavigationPlugInAttribute, PlugInAttribute, ToolbarPlugInAttribute, UrlRewriterModule

--- src/Core/N2DevelopmentWeb/Uc/ItemCreatorUI.ascx.cs
+++ src/Core/N2DevelopmentWeb/Uc/ItemCreatorUI.ascx.cs
@@ -28,9 +28,12 @@ namespace N2DevelopmentWeb.Uc
 			
 			for (int i = 0; i < count; i++)
 			{
-				ContentItem item = N2.Context.Definitions.CreateInstance<MyPageData>(parent);
+				MyPageData item = N2.Context.Definitions.CreateInstance<MyPageData>(parent);
 				item.Title = prefix + i;
 				item.Name = prefix + i;
+				item.Text = @"<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque placerat commodo turpis. Ut tempor interdum leo. In hac habitasse platea dictumst. Proin arcu velit, semper nec, feugiat vel, ultrices a, eros. Integer purus. Sed mollis tellus quis arcu. Nam ut erat. Integer purus. Integer dolor felis, porta ac, commodo ac, hendrerit vel, felis. Quisque cursus bibendum lorem. Curabitur cursus, magna et tincidunt bibendum, ante urna egestas orci, ut placerat arcu dolor a neque. Nulla dapibus tincidunt risus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque mattis. Donec tempor libero eget magna. Phasellus a mi et massa lobortis faucibus.</p>
+<p>Aenean convallis aliquet orci. Nullam posuere ante vitae libero. Sed vel lectus. Proin porta neque eget massa. Nulla vitae sapien. In placerat ante. In volutpat eros quis nisi placerat hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque dapibus nibh id turpis. Aenean semper fermentum dui. Vivamus sed lacus. Donec commodo diam sit amet tellus. Fusce sed justo quis purus mattis tristique. Suspendisse dapibus justo vitae libero. Mauris tempor tempus velit. Aenean aliquam hendrerit nisl. Nullam est metus, suscipit vitae, pulvinar a, mattis at, velit. Ut egestas. Vivamus massa dui, eleifend vitae, posuere sit amet, pulvinar ac, mi.</p>
+<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin id orci nec est dapibus pretium. Sed sagittis, tellus sit amet molestie pulvinar, mauris felis consequat augue, ac dictum nulla mauris eget lacus. Nullam viverra. Donec eleifend lectus sit amet nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras lacinia, nibh sed ornare lacinia, felis orci rhoncus mauris, ut cursus tellus ante ut dui. Integer dictum aliquet nibh. Nunc feugiat risus at nisl. Duis ac elit sit amet enim fermentum semper. Etiam ultrices mauris non turpis. Nulla facilisi. Donec ultrices commodo justo. Vestibulum sit amet lacus.</p>";
 				N2.Context.Persister.Save(item);
 				CreateItems(depth-1, count, item, prefix);
 			}
