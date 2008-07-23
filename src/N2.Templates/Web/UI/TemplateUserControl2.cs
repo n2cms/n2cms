5121e51513c27d0239d940e79e5a32d70f13e127
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2008-07-23 15:10:29 +0000

    - comments

--- src/N2.Templates/Web/UI/TemplateUserControl2.cs
+++ src/N2.Templates/Web/UI/TemplateUserControl2.cs
@@ -1,5 +1,14 @@
 namespace N2.Templates.Web.UI
 {
+    // Please note TemplateUserControl.cs with the static user control base class.
+
+    /// <summary>
+    /// A user control that can be dynamically added to a page and injected 
+    /// with the current content part data.
+    /// </summary>
+    /// <typeparam name="TPage">The type of page the user control is dynamically added to.</typeparam>
+    /// <typeparam name="TItem">The type of part the user control is a template for.</typeparam>
+    /// <remarks>This base class will be associated with the content data of the part which can be accessed through the CurrentItem property.</remarks>
 	public class TemplateUserControl<TPage, TItem> : N2.Web.UI.ContentUserControl<TPage, TItem>
 		where TPage : ContentItem
 		where TItem : ContentItem
