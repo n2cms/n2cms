6d7aad3b501329f977a132ddf81e58d4c602f454
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2008-11-08 22:14:55 +0000

    - updated assembly version

--- src/N2.Extensions/Properties/AssemblyInfo.cs
+++ src/N2.Extensions/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("8857e546-b0bb-43b8-9c6d-5fd31b9d82e2")]
 
 // Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0403.*")]
-[assembly: AssemblyFileVersion("1.4.3.1")]
+[assembly: AssemblyVersion("1.0.0404.*")]
+[assembly: AssemblyFileVersion("1.4.4")]
