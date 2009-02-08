0c8e7945ab73b1e2b1aacf0553758f1eb7a074f1
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-02-08 12:26:14 +0000

    - Adding missing SearchPage

---
+++ src/N2.Extensions.Tests/Mvc/Models/SearchPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace N2.Extensions.Tests.Mvc.Models
+{
+	public class SearchPage : ContentItem
+	{
+	}
+}
