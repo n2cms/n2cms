2bb8705bfe8a01854828affec6252a7f8848469e
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-03-12 22:59:46 +0000

    - fixed #21048: Languages and Recursive zones

--- src/wwwroot/Templates/Services/TemplatesPartsAspectController.cs
+++ src/wwwroot/Templates/Services/TemplatesPartsAspectController.cs
@@ -5,13 +5,17 @@ using N2.Web.Parts;
 
 namespace N2.Templates.Services
 {
+	/// <summary>
+	/// Implements "Recusive" zones functionality.
+	/// </summary>
 	[Controls(typeof(AbstractPage))]
 	public class TemplatesPartsAspectController : PartsAspectController
 	{
 		public override ItemList GetItemsInZone(ContentItem parentItem, string zoneName)
 		{
 			ItemList items =  base.GetItemsInZone(parentItem, zoneName);
-			if (zoneName.StartsWith("Recursive") && parentItem.Parent is AbstractContentPage)
+			ContentItem grandParentItem = parentItem;
+			if (zoneName.StartsWith("Recursive") && grandParentItem is AbstractContentPage && !(grandParentItem is LanguageRoot))
 			{
 				items.AddRange(GetItemsInZone(parentItem.Parent, zoneName));
 			}
