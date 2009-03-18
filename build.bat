6cdba1d4ec4517a5a71a8e262e2965a05c028a73
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-03-18 22:02:04 +0000

    - applied #20614: getting error when running Deploy_Everything-vs2008.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @ECHO Assuming msbuild location: %windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe
 
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe msbuild.proj /maxcpucount %*
+%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe msbuild.proj /maxcpucount /p:Configuration=Release;Platform="Any CPU" %*
