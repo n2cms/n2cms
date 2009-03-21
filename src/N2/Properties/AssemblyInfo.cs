c9d73ae4c2801846d53d5bcdc9b3d76c3dfa0cf1
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-03-21 22:12:52 +0000

    - updated version number

--- src/N2/Properties/AssemblyInfo.cs
+++ src/N2/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Security;
 
 // Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0405.*")]
-[assembly: AssemblyFileVersion("1.4.5")]
+[assembly: AssemblyFileVersion("1.4.5.1")]
 
 [assembly: CLSCompliant(true)]
 [assembly: AllowPartiallyTrustedCallers]
--- src/wwwroot/Templates/Properties/AssemblyInfo.cs
+++ src/wwwroot/Templates/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0405.*")]
-[assembly: AssemblyFileVersion("1.4.5")]
+[assembly: AssemblyFileVersion("1.4.5.1")]
