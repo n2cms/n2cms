3821256574384a4c743c1a4d715e849eb3b966e1
mrshrinkray <mrshrinkray@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-03-22 17:01:01 +0000

    - Added GoogleSiteMapHandler IHttpHandler and updated example.web.config to show its use

---
+++ src/N2/Web/GoogleSiteMapHandler.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Xml;
+using System.Text;
+using N2;
+using System.IO;
+using System.Web.Caching;
+
+namespace N2.Web
+{
+	/// <summary>
+	/// Provides a sitemap format file (XML) for the site, accessed via
+	/// http://www.yoursite.com/GoogleSiteMapHandler.axd
+	/// 
+	/// See sitemap.org for details on what's outputted.
+	/// </summary>
+	/// <remarks>
+	/// This doesn't handle multiple sites and a lot of settings are hardcoded for now.
+	/// 
+	/// You can ping google to update your sitemap (along with the webmaster tools) using
+	/// http://www.google.com/ping?sitemap=url
+	/// </remarks>
+	public class GoogleSiteMapHandler : IHttpHandler
+	{
+		public bool IsReusable
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		/// <summary>
+		/// Entry point for the handler.
+		/// </summary>
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.ClearHeaders();
+			context.Response.ClearContent();
+
+			context.Response.ContentType = "text/xml";
+
+			// The XML document is in UTF-16 but we're forcing utf-8 for google, which might be a problem
+			context.Response.Write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			context.Response.Write(GetSiteMap(context));
+		}
+
+		public string GetSiteMap(HttpContext context)
+		{
+			// As this is a heavy operation, use the cache
+			if (context.Cache["GoogleSitemap"] != null)
+				return context.Cache["GoogleSitemap"].ToString();
+
+			// TODO: Make sure this is correct
+			string domain = "http://" + context.Request.Url.Authority;
+
+			IList<ContentItem> list = new List<ContentItem>();
+			ContentItem rootItem = N2.Find.RootItem;
+			RecurseTree(list, rootItem);
+
+			StringBuilder builder = new StringBuilder();
+			StringWriter stringWriter = new StringWriter(builder);
+			using (XmlTextWriter writer = new XmlTextWriter(stringWriter))
+			{
+				writer.Settings.OmitXmlDeclaration = true;
+				writer.Formatting = Formatting.Indented;
+				writer.WriteStartDocument();
+
+				// <urlset>
+				writer.WriteStartElement("urlset");
+				writer.WriteAttributeString("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9"); // easier than using NS with the writer
+
+				foreach (var item in list)
+				{
+					// <url>
+					writer.WriteStartElement("url");
+
+					writer.WriteElementString("loc", domain + item.Url);
+					writer.WriteElementString("lastmod", item.Published.GetValueOrDefault().ToString("yyyy-MM-dd")); // Google doesn't like IS0 8601/W3C 
+					writer.WriteElementString("changefreq", "weekly"); // TODO make this a setting
+					writer.WriteElementString("priority", "0"); // TODO make this a setting
+
+					// </url>
+					writer.WriteEndElement();
+				}
+
+				// <urlset>
+				writer.WriteEndElement();
+			}
+
+			stringWriter.Flush();
+
+			// Add to the cache for 3 days.
+			context.Cache.Add("GoogleSitemap", builder.ToString(), null, DateTime.Today.AddDays(3), Cache.NoSlidingExpiration, CacheItemPriority.Normal, null);
+
+			return builder.ToString();
+		}
+
+		/// <summary>
+		/// Builds up the entire site tree recursively, adding items to the list
+		/// </summary>
+		/// <param name="list">This should be an empty list</param>
+		/// <param name="parent">This should be called using the root item</param>
+		private void RecurseTree(IList<ContentItem> list, ContentItem parent)
+		{
+			foreach (var item in parent.GetChildren())
+			{
+				if (item.Visible && item.IsPage) // TODO make these a setting
+				{
+					list.Add(item);
+					RecurseTree(list, item);
+				}
+			}
+		}
+	}
+}
