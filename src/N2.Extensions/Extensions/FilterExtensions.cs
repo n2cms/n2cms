8e63792b12ad9fba986fb34a7fe6ec443294ac92
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-05-09 19:30:02 +0000

    added missing file

---
+++ src/N2.Extensions/Extensions/FilterExtensions.cs
@@ -0,0 +1,48 @@
+﻿using N2.Collections;
+using System.Security.Principal;
+using N2.Security;
+
+namespace N2.Extensions
+{
+	public static class FilterExtensions
+	{
+		/// <summary>Filters an item by access which returning null if the item is not accessible to the current user.</summary>
+		/// <typeparam name="T">Generic argument to allow strongly typed returns.</typeparam>
+		/// <param name="item">The item to filter.</param>
+		/// <returns>The item if it's accessible to the current user.</returns>
+		public static T FilterByAccess<T>(this T item) where T: ContentItem
+		{
+			return item.FilterWith(new AccessFilter());
+		}
+
+		/// <summary>Filters an item by access which returning null if the item is not accessible to the current user.</summary>
+		/// <typeparam name="T">Generic argument to allow strongly typed returns.</typeparam>
+		/// <param name="item">The item to filter.</param>
+		/// <param name="user">The user to filter access for.</param>
+		/// <returns>The item if it's accessible to the current user.</returns>
+		public static T FilterByAccess<T>(this T item, IPrincipal user) where T : ContentItem
+		{
+			return item.FilterWith(new AccessFilter(user, AccessFilter.CurrentSecurityManager()));
+		}
+
+		/// <summary>Filters an item by it's potential visibility in the menu which returning null if the item is not accessible to the current user.</summary>
+		/// <typeparam name="T">Generic argument to allow strongly typed returns.</typeparam>
+		/// <param name="item">The item to filter.</param>
+		/// <returns>The item if it's accessible to the current user.</returns>
+		public static T FilterByNavigation<T>(this T item) where T : ContentItem
+		{
+			return item.FilterWith(new NavigationFilter());
+		}
+
+		private static T FilterWith<T>(this T item, ItemFilter filter) where T: ContentItem
+		{
+			if (item == null)
+				return null;
+
+			if (filter.Match(item))
+				return item;
+
+			return null;
+		}
+	}
+}
