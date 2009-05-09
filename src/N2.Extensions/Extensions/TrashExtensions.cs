522ca56880916f931d81b23f8f0249db438316b9
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-05-09 16:54:17 +0000

    - copied esteewhy's IsRecycled extension method

---
+++ src/N2.Extensions/Extensions/TrashExtensions.cs
@@ -0,0 +1,22 @@
+﻿using N2.Edit.Trash;
+
+namespace N2.Extensions
+{
+	public static class TrashExtensions
+	{
+		/// <summary>
+		/// Checks if the item contained within a Trash Can.
+		/// </summary>
+		/// <param name="item">The item to check.</param>
+		/// <returns>True if the item is in the trash can.</returns>
+		public static bool IsRecycled(this ContentItem item)
+		{
+			foreach(ContentItem ancestor in Find.EnumerateParents(item))
+			{
+				if (ancestor is ITrashCan)
+					return true;
+			}
+			return false;
+		}
+	}
+}
