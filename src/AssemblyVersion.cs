514327279639be4071ba8317cee00efaeb24b336
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-06-06 17:03:33 +0000

    - updated assembly version

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 // Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0405.*")]
-[assembly: AssemblyFileVersion("1.4.5.3")]
+[assembly: AssemblyVersion("1.0.0500.*")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
