d88eddb389533271c0a9ccbc9ee65b5112dab4e3
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-11-03 20:37:53 +0000

    - fixed #25076: n2 doesn't compile under vs2010 beta 2

--- src/N2/Engine/Globalization/DictionaryScope.cs
+++ src/N2/Engine/Globalization/DictionaryScope.cs
@@ -5,24 +5,31 @@ using System.Collections;
 
 namespace N2.Engine.Globalization
 {
-	public class DictionaryScope : Scope
+	public class DictionaryScope : IDisposable
 	{
+        IDictionary dictionary;
 		object previousValue = null;
+        object key;
 
 		public DictionaryScope(IDictionary dictionary, object key, object value)
-			: base(delegate
-			{
-				if (dictionary.Contains(key))
-					previousValue = dictionary[key];
-				dictionary[key] = value;
-			}, delegate
-			{
-				if (previousValue == null)
-					dictionary.Remove(key);
-				else
-					dictionary[key] = previousValue;
-			})
-		{
+        {
+            if (dictionary.Contains(key))
+				previousValue = dictionary[key];
+			dictionary[key] = value;
+            this.key = key;
+            this.dictionary = dictionary;
 		}
-	}
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (previousValue == null)
+                dictionary.Remove(key);
+            else
+                dictionary[key] = previousValue;
+        }
+
+        #endregion
+    }
 }
