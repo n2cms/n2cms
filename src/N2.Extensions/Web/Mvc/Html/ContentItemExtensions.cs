726c4c759a9b46210906b52e2f4b097710e9a616
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2009-12-13 19:32:42 +0000

    missing file

---
+++ src/N2.Extensions/Web/Mvc/Html/ContentItemExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace N2.Web.Mvc.Html
+{
+    public static class ContentItemExtensions
+    {
+        public static ContentItem CurrentItem(this HtmlHelper helper)
+        {
+            return helper.ViewContext.CurrentItem();
+        }
+
+        public static ContentItem CurrentItem(this ViewContext context)
+        {
+            return context.ViewData[ContentRoute.ContentItemKey] as ContentItem
+                ?? context.RequestContext.CurrentItem();
+        }
+
+        public static ContentItem CurrentItem(this RequestContext context)
+        {
+            return context.RouteData.Values[ContentRoute.ContentItemKey] as ContentItem;
+        }
+    }
+}
