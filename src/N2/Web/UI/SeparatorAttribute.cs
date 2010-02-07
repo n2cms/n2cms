9baaf3e7f706cdf535761416006cca400e394ed1
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2010-02-07 12:27:20 +0000

    missing file

---
+++ src/N2/Web/UI/SeparatorAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using N2.Definitions;
+using N2.Web.UI.WebControls;
+
+namespace N2.Web.UI
+{
+	/// <summary>
+	/// Creates a separator that can be ordered between two editors
+	/// </summary>
+	public class SeparatorAttribute : EditorContainerAttribute
+	{
+		public SeparatorAttribute(string name, int sortOrder)
+			: base(name, sortOrder)
+		{
+		}
+
+		public override System.Web.UI.Control AddTo(System.Web.UI.Control container)
+		{
+			var hr = new Hr();
+			container.Controls.Add(hr);
+			return hr;
+		}
+	}
+}
