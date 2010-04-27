494b431b13dcfc35dd93531ed0f7b4fbe9236b54
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2010-04-27 18:56:10 +0000

    - Applied meixger's patch #5854: More character replacements for german umlaute and italian accents & apostrophes

--- src/N2/Configuration/PatternValueCollection.cs
+++ src/N2/Configuration/PatternValueCollection.cs
@@ -9,12 +9,23 @@ namespace N2.Configuration
 	{
 		public PatternValueCollection()
 		{
-			BaseAdd(new PatternValueElement("smallA", "[åäáàâã@]", "a", true));
-			BaseAdd(new PatternValueElement("capitalA", "[ÅÄÁÀÂÃ]", "a", true));
-			BaseAdd(new PatternValueElement("smallAE", "[æ]", "ae", true));
-			BaseAdd(new PatternValueElement("capitalAE", "[Æ]", "AE", true));
-			BaseAdd(new PatternValueElement("smallO", "[öøôõ]", "o", true));
-			BaseAdd(new PatternValueElement("capitalO", "[ÖØÔÕ]", "O", true));
+			BaseAdd(new PatternValueElement("smallA", "[åáàâã@]", "a", true));
+			BaseAdd(new PatternValueElement("capitalA", "[ÅÁÀÂÃ]", "a", true));
+			BaseAdd(new PatternValueElement("smallAE", "[æä]", "ae", true));
+			BaseAdd(new PatternValueElement("capitalAE", "[ÆÄ]", "Ae", true));
+			BaseAdd(new PatternValueElement("smallE", "[éè]", "e", true));
+			BaseAdd(new PatternValueElement("capitalE", "[ÉÈ]", "E", true));
+			BaseAdd(new PatternValueElement("smallI", "[íì]", "i", true));
+			BaseAdd(new PatternValueElement("capitalI", "[ÍÌ]", "I", true));
+			BaseAdd(new PatternValueElement("smallO", "[øóòôõ]", "o", true));
+			BaseAdd(new PatternValueElement("capitalO", "[ØÓÒÔÕ]", "O", true));
+			BaseAdd(new PatternValueElement("smallOE", "[ö]", "oe", true));
+			BaseAdd(new PatternValueElement("capitalOE", "[Ö]", "Oe", true));
+			BaseAdd(new PatternValueElement("smallU", "[úù]", "u", true));
+			BaseAdd(new PatternValueElement("capitalU", "[ÚÙ]", "U", true));
+			BaseAdd(new PatternValueElement("smallUE", "[ü]", "ue", true));
+			BaseAdd(new PatternValueElement("capitalUE", "[Ü]", "Ue", true));
+			BaseAdd(new PatternValueElement("germanSZ", "[ß]", "ss", true));
 			BaseAdd(new PatternValueElement("theRest", "[^. a-zA-Z0-9_-]", "", true));
 		}
 
