efc4acf0d10956369a5481f9d2f0b2aa647afd4e
cristian.libardo <cristian.libardo@0cd172cf-a144-0410-9a69-6b897155a35c>
2010-06-06 10:09:55 +0000

    Refactored assembly information in template projects

--- src/wwwroot/Templates/Properties/AssemblyInfo.cs
+++ src/wwwroot/Templates/Properties/AssemblyInfo.cs
@@ -3,17 +3,15 @@ using System.Runtime.InteropServices;
 using N2.Web;
 
 // General Information about an assembly is controlled through the following set of attributes. Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("N2.Templates.UI")]
+[assembly: AssemblyTitle("N2.Templates.WebForms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("N2.Templates.UI")]
-[assembly: AssemblyCopyright("Copyright © 2006-2009 Crisitan Libardo")]
+[assembly: AssemblyProduct("N2.Templates.WebForms")]
+[assembly: AssemblyCopyright("Copyright © 2006-2010 N2CMS")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM components.  If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4292cddf-3e65-461f-91f4-e891a9aef531")]
+// Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
 
 [assembly: ConventionTemplateDirectory("~/Templates/UI/Views/")]
