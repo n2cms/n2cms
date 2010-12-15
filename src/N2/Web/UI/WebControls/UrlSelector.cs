10e0b4dc437a07966eac33660436cda02c1d216b
libardo <cristianlibardo@hotmail.com>
2010-12-15 22:54:39 +0100

    Applied MingStar's fix to {ManagementUrl} in UrlSelector.BrowserUrl

--- src/N2/Web/UI/WebControls/UrlSelector.cs
+++ src/N2/Web/UI/WebControls/UrlSelector.cs
@@ -124,7 +124,7 @@ function openUrlSelectorPopup(popupUrl,tbId,popupOptions,defaultMode,availableMo
 			hib.Attributes["class"] = "popupButton selectorButton";
 			Controls.Add(hib);
 			hib.Attributes["onclick"] = string.Format(OpenPopupFormat,
-													  N2.Web.Url.ToAbsolute(BrowserUrl),
+													  N2.Web.Url.ResolveTokens(BrowserUrl),
 													  ClientID,
 													  PopupOptions,
 													  DefaultMode,
