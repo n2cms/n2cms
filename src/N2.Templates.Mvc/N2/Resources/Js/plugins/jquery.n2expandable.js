78c5432f6c3fb0913feb2fdd50840c02da067902
libardo <cristianlibardo@hotmail.com>
2010-12-25 23:57:31 +0100

    - Fixed #29845: Legend error with multiple ExpandableContainer items

--- src/N2.Templates.Mvc/N2/Resources/Js/plugins/jquery.n2expandable.js
+++ src/N2.Templates.Mvc/N2/Resources/Js/plugins/jquery.n2expandable.js
@@ -1,61 +1,49 @@
-﻿(function($) {
-    $.fn.n2expandable = function(args) {
-        var $children = this.children();
-        if (args.visible) {
-            $children = $children.not(args.visible);
-            this.visible = args.visible;
-        }
+﻿(function ($) {
+	$.fn.n2expandable = function (args) {
+		this.each(function () {
+			$panel = $(this);
+			var $children = $panel.children();
+			if (args.visible) {
+				$children = $children.not(args.visible);
+			}
 
-        if ($children.length == 0)
-            return;
+			if ($children.length == 0)
+				return;
 
-        var text = "Details";
-        if (text = this.attr("title"))
-            this.attr("title", "");
+			var text = "Details";
+			if (text = $panel.attr("title"))
+				$panel.attr("title", "");
 
-        var $expander = (args.expander)
-			? $(args.expander)
-			: $("<a href='#' class='expander'>" + text + "</a>");
+			var $expander = (args.expander)
+				? $(args.expander)
+				: $("<a href='#' class='expander'>" + text + "</a>");
 
-        $expander.prependTo(this);
+			$expander.prependTo($panel);
 
-        var self = this;
+			function show() {
+				$children.fadeIn();
+				$panel.removeClass('expandable-contracted').addClass('expandable-expanded');
+			}
 
-        this.find('.expander').click(function(e) {
-            if ($(this).parent().is('.expandable-expanded')) {
-                if (self.visible) {
-                    $(this).parent().children().not('.expander').not(self.visible).hide();
-                }
-                else {
-                    $(this).parent().children().not('.expander').hide();
-                }
-                $(this).parent().removeClass('expandable-expanded').addClass('expandable-contracted');
-            }
-            else {
-                if (self.visible) {
-                    $(this).parent().children().not('.expander').not(self.visible).fadeIn();
-                }
-                else {
-                    $(this).parent().children().not('.expander').fadeIn();
-                }
-                $(this).parent().removeClass('expandable-contracted').addClass('expandable-expanded');
-            }
-            e.preventDefault();
-            e.stopPropagation();
-        });
-        this.click(function(e) {
-            if (!$(this).is(".expandable-expanded")) {
-                if (self.visible) {
-                    $(this).children().not('.expander').not(self.visible).fadeIn();
-                }
-                else {
-                    $(this).children().not('.expander').fadeIn();
-                }
-                $(this).removeClass('expandable-contracted').addClass('expandable-expanded');
-            }
-        });
+			$panel.find('.expander').click(function (e) {
+				if ($panel.is('.expandable-expanded')) {
+					$children.hide();
+					$panel.removeClass('expandable-expanded').addClass('expandable-contracted');
+				} else {
+					show();
+				}
+				e.preventDefault();
+				e.stopPropagation();
+			});
 
-        $children.hide();
-        this.addClass("expandable-contracted");
-    };
-})(jQuery);;
+			$panel.click(function (e) {
+				if (!$panel.is(".expandable-expanded")) {
+					show();
+				}
+			});
+
+			$children.hide();
+			$panel.addClass("expandable-contracted");
+		});
+	};
+})(jQuery);
