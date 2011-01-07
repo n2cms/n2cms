0580241b2813f123e502b2e99acf00ef83be70e1
libardo <cristianlibardo@hotmail.com>
2011-01-08 00:22:40 +0100

    Fixed some view compilation issues reveled in web site project

--- src/Mvc/MvcTemplates/Models/Pages/GalleryItem.cs
+++ src/Mvc/MvcTemplates/Models/Pages/GalleryItem.cs
@@ -44,26 +44,6 @@ namespace N2.Templates.Mvc.Models.Pages
 			return ImageUrl;
 		}
 
-		///// <summary>Returns the path to an image handler that resizes the given image to the appropriate size.</summary>
-		///// <param name="imageUrl">The image to resize.</param>
-		///// <param name="width">The maximum width.</param>
-		///// <param name="height">The maximum height.</param>
-		///// <returns>The path to a handler that performs resizing of the image.</returns>
-		//public static string GetResizedImageUrl(string imageUrl, double width, double height)
-		//{
-		//    // TODO: Refactor to HtmlHelper extension
-		//    string fileExtension = VirtualPathUtility.GetExtension(N2.Web.Url.PathPart(imageUrl));
-		//    bool isAlreadyImageHandler = string.Equals(fileExtension, ".ashx", StringComparison.OrdinalIgnoreCase);
-
-		//    if (isAlreadyImageHandler) return N2.Web.Url.ToAbsolute(imageUrl);
-
-		//    Url url = new Url("~/Image.ashx").SetQueryParameter("img", N2.Web.Url.ToAbsolute(imageUrl));
-		//    if (width > 0) url = url.SetQueryParameter("w", (int) width);
-		//    if (height > 0) url = url.SetQueryParameter("h", (int) height);
-
-		//    return url;
-		//}
-
 		public virtual ImageGallery Gallery
 		{
 			get { return Parent as ImageGallery; }
