e1b9766a4a16631d61041c27b8d287594616ba72
libardo <cristianlibardo@hotmail.com>
2011-01-10 21:59:53 +0100

    Tweaked diagnose page

--- src/N2.Templates.Mvc/N2/Installation/Diagnose.aspx.cs
+++ src/N2.Templates.Mvc/N2/Installation/Diagnose.aspx.cs
@@ -221,5 +221,28 @@ namespace N2.Edit.Install
 			return
 				string.Format("<textarea>{0}\n\n{1}</textarea>", ex.Message, ex.StackTrace);
 		}
+
+		/// <summary>
+		/// Finds the trust level of the running application (http://blogs.msdn.com/dmitryr/archive/2007/01/23/finding-out-the-current-trust-level-in-asp-net.aspx)
+		/// </summary>
+		/// <returns>The current trust level.</returns>
+		protected static AspNetHostingPermissionLevel GetTrustLevel()
+		{
+			foreach (AspNetHostingPermissionLevel trustLevel in new[] { AspNetHostingPermissionLevel.Unrestricted, AspNetHostingPermissionLevel.High, AspNetHostingPermissionLevel.Medium, AspNetHostingPermissionLevel.Low, AspNetHostingPermissionLevel.Minimal })
+			{
+				try
+				{
+					new AspNetHostingPermission(trustLevel).Demand();
+				}
+				catch (System.Security.SecurityException)
+				{
+					continue;
+				}
+
+				return trustLevel;
+			}
+
+			return AspNetHostingPermissionLevel.None;
+		}
 	}
 }
