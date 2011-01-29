ed220979c053134056b8147d2f497f65e370a414
libardo <cristianlibardo@hotmail.com>
2011-01-29 22:25:23 +0100

    Updated start page model in dinamico to include marker interfaces

--- src/Mvc/Dinamico/Models/StartPage.cs
+++ src/Mvc/Dinamico/Models/StartPage.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using N2;
+using N2.Integrity;
+using N2.Definitions;
 
 namespace Dinamico.Models
 {
 	[PageDefinition]
-	public class StartPage : TextPage
+	[RestrictParents(typeof(IRootPage))]
+	public class StartPage : TextPage , IStartPage, IStructuralPage
 	{
 	}
 }
