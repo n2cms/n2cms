999de5273129f1a31473290c194d839c38a7c660
libardo <cristianlibardo@hotmail.com>
2011-03-06 22:30:26 +0100

    Added concrete theme to dinamico

--- src/Mvc/Dinamico/Details/EditableThemeAttribute.cs
+++ src/Mvc/Dinamico/Details/EditableThemeAttribute.cs
@@ -12,19 +12,16 @@ namespace N2.Details
 	{
 		public EditableThemeAttribute()
 		{
-			DefaultValue = "Default";
-			Required = true;
 		}
 
 		protected override ListItem[] GetListItems()
 		{
-			List<ListItem> items = new List<ListItem> { new ListItem("", "Default") };
+			List<ListItem> items = new List<ListItem>();
 
 			var dir = HostingEnvironment.VirtualPathProvider.GetDirectory("~/Themes/");
 			if (dir != null)
 			{
 				items.AddRange(dir.Directories.OfType<VirtualDirectory>()
-					.Where(d => !d.Name.Equals("Default", StringComparison.InvariantCultureIgnoreCase))
 					.Select(d => new ListItem(d.Name)));
 			}
 
