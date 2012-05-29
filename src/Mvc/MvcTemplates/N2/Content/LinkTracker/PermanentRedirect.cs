141f42b4bfb63635976141498f2c623a2605fc21
Cristian Libardo <cristianlibardo@hotmail.com>
2012-05-30 00:07:07 +0200

    Made permanent redirecet less obtrusive

--- src/Mvc/MvcTemplates/N2/Content/LinkTracker/PermanentRedirect.cs
+++ src/Mvc/MvcTemplates/N2/Content/LinkTracker/PermanentRedirect.cs
@@ -5,12 +5,18 @@ using System.Web;
 using N2;
 using N2.Definitions;
 using N2.Details;
+using N2.Persistence.Search;
+using N2.Installation;
 
 namespace N2.Management.Content.LinkTracker
 {
 	[PageDefinition(IconUrl = "{IconsUrl}/error_go.png",
-		TemplateUrl = "{ManagementUrl}/Resources/RedirectHandler.ashx")]
-	public class PermanentRedirect : ContentItem, IRedirect
+		InstallerVisibility = InstallerHint.NeverRootOrStartPage,
+		TemplateUrl = "{ManagementUrl}/Resources/RedirectHandler.ashx",
+		AuthorizedRoles = new string[0])]
+	[Indexable(IsIndexable = false)]
+	[Throwable(AllowInTrash.No)]
+	public class PermanentRedirect : ContentItem, IRedirect, ISystemNode
 	{
 		public PermanentRedirect()
 		{
