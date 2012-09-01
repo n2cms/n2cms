d71665015536d7a030dac005c9420331f0df8db0
Cristian Libardo <cristianlibardo@hotmail.com>
2012-09-01 10:39:42 +0200

    Model tweak

--- src/Mvc/MvcTemplates/Models/Parts/PartBase.cs
+++ src/Mvc/MvcTemplates/Models/Parts/PartBase.cs
@@ -1,11 +1,12 @@
 using System;
+using N2.Definitions;
 
 namespace N2.Templates.Mvc.Models.Parts
 {
 	/// <summary>
 	/// A base class for item parts in the templates project.
 	/// </summary>
-	public abstract class PartBase : ContentItem
+	public abstract class PartBase : ContentItem, IPart
 	{
 	}
 
