8340f5eb6549de555bfd3dc1ad58650a8f97da73
Magnus von Wachenfeldt <magnus@deckhand.se>
2012-09-24 20:01:48 +0200

    This is not the push you're looking for

--- src/Framework/N2/Web/UI/WebControls/Zone.cs
+++ src/Framework/N2/Web/UI/WebControls/Zone.cs
@@ -157,7 +157,7 @@ namespace N2.Web.UI.WebControls
 
         protected virtual string GetInterface()
         {
-            return System.Web.HttpContext.Current.Request.QueryString["draft"] == "true" ? "Draft" : Interfaces.Viewing;
+            return Interfaces.Viewing;
         }
 
 		protected virtual void OnSelecting(ItemListEventArgs args)
