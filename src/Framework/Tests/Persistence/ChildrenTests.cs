d2279b91d33af71b5c0144482f8ab9e8e927f35d
Cristian Libardo <cristianlibardo@hotmail.com>
2012-10-20 21:23:21 +0200

    Tweaked test

--- src/Framework/Tests/Persistence/ChildrenTests.cs
+++ src/Framework/Tests/Persistence/ChildrenTests.cs
@@ -84,13 +84,14 @@ namespace N2.Tests.Persistence
 			Find.EnumerateChildren(parent).Count().ShouldBe(4);
 		}
 
-		[TestCase(10)]
+		[TestCase(1)]
+		//[TestCase(10)]
 		//[TestCase(100)]
 		//[TestCase(1000)]
 		//[TestCase(10000)]
 		public void Save_ManyAssociations_InSession(int iterations)
 		{
-			for (int i = 0; i < 100; i++)
+			for (int i = 0; i < iterations; i++)
 			{
 				var parent = new PersistableItem1();
 				persister.Save(parent);
@@ -104,11 +105,11 @@ namespace N2.Tests.Persistence
 				persister.Save(child1_1);
 
 				var child2 = new PersistableItem1 { Parent = parent };
-				child1_1["sibling"] = child1;
+				child2["sibling"] = child1;
 				persister.Save(child2);
 
 				var child2_1 = new PersistableItem1 { Parent = child2 };
-				child1_1["cousin"] = child1_1;
+				child2_1["cousin"] = child1_1;
 				persister.Save(child2_1);
 
 				child1["child"] = child1_1;
@@ -122,6 +123,71 @@ namespace N2.Tests.Persistence
 				persister.Save(child2);
 
 				Find.EnumerateChildren(parent).Count().ShouldBe(4);
+			}
+		}
+
+		[Test]
+		public void MultipleSessions()
+		{
+			for (int i = 0; i < 1; i++)
+			{
+				var parent = new PersistableItem1();
+				persister.Save(parent);
+
+				persister.Dispose();
+				parent = persister.Get<PersistableItem1>(parent.ID);
+
+				var child1 = new PersistableItem1 { Parent = parent };
+				child1["parent"] = parent;
+				persister.Save(child1);
+
+				var child1_1 = new PersistableItem1 { Parent = child1 };
+				child1_1["parent"] = child1_1;
+				persister.Save(child1_1);
+
+				persister.Dispose();
+
+				parent = persister.Get<PersistableItem1>(parent.ID);
+				child1 = persister.Get<PersistableItem1>(child1.ID);
+				child1_1 = persister.Get<PersistableItem1>(child1_1.ID);
+
+				var child2 = new PersistableItem1 { Parent = parent };
+				child2.ZoneName = "TheZone";
+				child2["sibling"] = child1;
+				persister.Save(child2);
+
+				var child2_1 = new PersistableItem1 { Parent = child2 };
+				child2_1.ZoneName = "TheZone";
+				child2_1["cousin"] = child1_1;
+				persister.Save(child2_1);
+
+				persister.Dispose();
+
+				parent = persister.Get<PersistableItem1>(parent.ID);
+				child1 = persister.Get<PersistableItem1>(child1.ID);
+				child1_1 = persister.Get<PersistableItem1>(child1_1.ID);
+				child2 = persister.Get<PersistableItem1>(child2.ID);
+				child2_1 = persister.Get<PersistableItem1>(child2_1.ID);
+
+				child1["child"] = child1_1;
+				child1["sibling"] = child2;
+				persister.Save(child1);
+
+				child1_1["cousin"] = child2_1;
+				persister.Save(child1_1);
+
+				child2["child"] = child2_1;
+				persister.Save(child2);
+
+				persister.Dispose();
+
+				persister.Get(parent.ID).Children.FindPages().Count().ShouldBe(1);
+				persister.Get(parent.ID).Children.FindParts().Count().ShouldBe(1);
+
+				persister.Get(parent.ID).Children.Select(c => c.ID).Contains(child1.ID).ShouldBe(true);
+				persister.Get(parent.ID).Children.Select(c => c.ID).Contains(child2.ID).ShouldBe(true);
+
+				persister.Get(child1.ID)["child"].ShouldBe(persister.Get(child1_1.ID));
 			}
 		}
 	}
