f4a14fa20e12a66b5a36cafd344d796e4363738d
Cristian Libardo <cristianlibardo@hotmail.com>
2012-10-30 23:04:30 +0100

    Test parts for editable item and editable children

--- src/Mvc/MvcTemplates/Areas/Tests/Models/TestPart.cs
+++ src/Mvc/MvcTemplates/Areas/Tests/Models/TestPart.cs
@@ -7,14 +7,23 @@ using System.Web;
 
 namespace N2.Templates.Mvc.Areas.Tests.Models
 {
+    [PartDefinition(Title = "TestPart [child]", SortOrder = 21002)]
+    public class TestPartWithEditableChild : TestPart
+    {
+        [EditableItem("EditableItem", 100)]
+        public virtual TestWebFormPart EditableItem { get; set; }
+    }
+
+    [PartDefinition(Title = "TestPart [children]", SortOrder = 21003)]
+    public class TestPartWithEditableChildren : TestPart
+    {
+        [EditableChildren("Children", "EditableChildren", 200)]
+        public virtual IEnumerable<TestWebFormPart> EditableChildren { get; set; }
+    }
+
 	[PartDefinition(SortOrder = 21001)]
 	public class TestPart : TestItemBase
 	{
-        [EditableItem("EditableChild", 100)]
-        public virtual TestWebFormPart EditableItem { get; set; }
-
-        [EditableChildren("Children", "Children", 200)]
-        public virtual IEnumerable<TestWebFormPart> EditableChildren { get; set; }
 	}
 }
 #endif
--- src/Mvc/MvcTemplates/Areas/Tests/Views/TestPart/Index.ascx
+++ src/Mvc/MvcTemplates/Areas/Tests/Views/TestPart/Index.ascx
@@ -1,6 +1,10 @@
 ﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<IEnumerable<N2.Definitions.ItemDefinition>>" %>
+<style>
+    .hoverExpand .inner { display:none; }
+    .hoverExpand:hover .inner { display:block; }
+</style>
 <div class="uc">
-	<div class="box">
+	<div class="box hoverExpand">
 		<h4>Test Part</h4>
 		<div class="inner">
 			<h5>Route Values</h5>
@@ -21,3 +25,19 @@
 		<input type="submit" value="Remove this" style="font-size:.7em" />
 	<% } %>
 </div>
+<table>
+    <tr>
+        <th>current</th><td><%= Html.CurrentItem() %></td>
+    </tr>
+    <tr>
+        <th>editable item</th><td><%= Html.CurrentItem()["EditableItem"] %></td>
+    </tr>
+    <tr>
+        <th>editable children</th>
+        <td>
+            <% foreach(var child in (IEnumerable)(Html.CurrentItem()["EditableChildren"] ?? new N2.ContentItem[0])) { %>
+            <%= child %><br />
+            <% } %>
+        </td>
+    </tr>
+</table>
