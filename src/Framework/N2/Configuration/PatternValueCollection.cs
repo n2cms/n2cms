b0b0c1ef44c797f8be74b1a8536a887d7559e309
Dejan Milicic <dejan.milicic@gmail.com>
2013-05-19 22:07:31 +0200

    Server-side implementation of slug generation and validation

--- src/Framework/N2/Configuration/PatternValueCollection.cs
+++ src/Framework/N2/Configuration/PatternValueCollection.cs
@@ -10,24 +10,13 @@ namespace N2.Configuration
 	{
 		public PatternValueCollection()
 		{
-			AddDefault(new PatternValueElement("smallA", "[åáàâã@]", "a", true));
-			AddDefault(new PatternValueElement("capitalA", "[ÅÁÀÂÃ]", "a", true));
+			AddDefault(new PatternValueElement("At", "[@]", "a", true));
 			AddDefault(new PatternValueElement("smallAE", "[æä]", "ae", true));
 			AddDefault(new PatternValueElement("capitalAE", "[ÆÄ]", "Ae", true));
-			AddDefault(new PatternValueElement("smallE", "[éè]", "e", true));
-			AddDefault(new PatternValueElement("capitalE", "[ÉÈ]", "E", true));
-			AddDefault(new PatternValueElement("smallI", "[íì]", "i", true));
-			AddDefault(new PatternValueElement("capitalI", "[ÍÌ]", "I", true));
-			AddDefault(new PatternValueElement("smallO", "[øóòôõ]", "o", true));
-			AddDefault(new PatternValueElement("capitalO", "[ØÓÒÔÕ]", "O", true));
 			AddDefault(new PatternValueElement("smallOE", "[ö]", "oe", true));
 			AddDefault(new PatternValueElement("capitalOE", "[Ö]", "Oe", true));
-			AddDefault(new PatternValueElement("smallU", "[úù]", "u", true));
-			AddDefault(new PatternValueElement("capitalU", "[ÚÙ]", "U", true));
 			AddDefault(new PatternValueElement("smallUE", "[ü]", "ue", true));
 			AddDefault(new PatternValueElement("capitalUE", "[Ü]", "Ue", true));
-			AddDefault(new PatternValueElement("germanSZ", "[ß]", "ss", true));
-			AddDefault(new PatternValueElement("theRest", "[^. a-zA-Z0-9_-]", "", true));
 		}
 	}
 }
