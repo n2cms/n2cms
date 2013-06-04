ca44d456e0a800c7739f66da54267fdcf768256c
Cristian Libardo <cristianlibardo@hotmail.com>
2013-06-04 23:23:37 +0200

    Fix empty actions on trash

--- src/Mvc/MvcTemplates/N2/App/Js/Controllers.js
+++ src/Mvc/MvcTemplates/N2/App/Js/Controllers.js
@@ -226,12 +226,18 @@ function PageActionBarCtrl($scope, $rootScope, Security) {
 	});
 
 	$scope.isDisplayable = function (item) {
-		if ($scope.Context.CurrentItem && !Security.permissions.is(item.Current.RequiredPermission, $scope.Context.CurrentItem.MaximumPermission))
+		if ($scope.Context.CurrentItem && !Security.permissions.is(item.Current.RequiredPermission, $scope.Context.CurrentItem.MaximumPermission)) {
+			console.log("unauthorized", item);
 			return false;
-		if (item.Current.HiddenBy)
+		}
+		if (item.Current.HiddenBy) {
+			console.log("hidden by", item.Current.HiddenBy, item);
 			return !$scope.isFlagged(item.Current.HiddenBy);
-		if (item.Current.DisplayedBy)
+		}
+		if (item.Current.DisplayedBy) {
+			console.log("displayed by", item.Current.DisplayedBy, $scope.isFlagged(item.Current.DisplayedBy), item);
 			return $scope.isFlagged(item.Current.DisplayedBy);
+		}
 		return true;
 	};
 	
