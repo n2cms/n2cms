a43ce99c5ba5db3e03f88cfa3dfca2e958896d50
Cristian Libardo <cristianlibardo@hotmail.com>
2013-06-23 21:54:53 +0200

    Targeting configuration

--- src/Mvc/MvcTemplates/N2/Targeting/Js/Targeting.js
+++ src/Mvc/MvcTemplates/N2/Targeting/Js/Targeting.js
@@ -1,19 +1,38 @@
 ﻿function TargetingCtrl($rootScope, $scope) {
+	$rootScope.$on("contextchanged", function (scope, e) {
+		if ($scope.Context.Flags.indexOf('Management') >= 0)
+			delete $scope.preview;
+	});
+
+	function rotate() {
+		var s = $scope.preview.size;
+		var newDimensions = { Height: s.Width, Width: s.Height };
+		angular.extend(s, newDimensions);
+	}
 
 	$rootScope.$on("device-preview", function (e, size) {
+		if ($scope.preview && $scope.preview.size && $scope.preview.size.Height == size.Height && $scope.preview.size.Width == size.Width) {
+			rotate();
+			return;
+		}
 		$scope.preview = {
 			size: size,
-			url: window.frames.preview && window.frames.preview.window.location.toString(),
-			horizontal: size.w > size.h
+			url: window.frames.preview && window.frames.preview.window.location.toString()
 		};
 		console.log($scope.preview);
 	});
-	$rootScope.$on("device-restore", function (e, size) {
+	$rootScope.$on("device-close", function (e, size) {
 		delete $scope.preview;
 	});
-	$rootScope.$on("resized", function () {
-		$scope.preview.size.title = "Custom";
+	$rootScope.$on("device-rotate", function (e, size) {
+		if ($scope.preview) {
+			rotate();
+		}
 	});
+	$rootScope.$on("resized", function () {
+		$scope.preview.size.Title = "Custom";
+	});
+
 	$scope.frameLoaded = function (e) {
 		try {
 			var loco = e.target.contentWindow.location;
--- src/Mvc/MvcTemplates/N2/Targeting/TargetingModule.cs
+++ src/Mvc/MvcTemplates/N2/Targeting/TargetingModule.cs
@@ -1,4 +1,5 @@
-﻿using N2.Management.Api;
+﻿using N2.Configuration;
+using N2.Management.Api;
 using N2.Web;
 using System;
 using System.Collections.Generic;
@@ -10,8 +11,11 @@ namespace N2.Management.Targeting
 	[ManagementModule]
 	public class TargetingModule : ManagementModuleBase
 	{
-		public TargetingModule(InterfaceBuilder builder)
+		private ConfigurationManagerWrapper config;
+
+		public TargetingModule(InterfaceBuilder builder, ConfigurationManagerWrapper config)
 		{
+			this.config = config;
 			builder.InterfaceBuilt += builder_InterfaceBuilt;
 		}
 
@@ -19,18 +23,20 @@ namespace N2.Management.Targeting
 		{
 			e.Data.Partials.Preview = "{ManagementUrl}/Targeting/Partials/DevicePreview.html".ResolveUrlTokens();
 			e.Data.ActionMenu.Add("preview",
-				new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "devicepreview", Title = "Device preview", IconClass = "n2-icon-mobile-phone" })
+				new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "devicepreview", Title = "Device preview", IconClass = "n2-icon-mobile-phone", TemplateUrl = "{ManagementUrl}/Targeting/Partials/DeviceMenu.html".ResolveUrlTokens() })
 				{
-					Children = new Node<InterfaceMenuItem>[]
-					{
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "pc", IconClass = "n2-icon-laptop", Title = "PC", ClientAction = "$emit('device-restore', null)" }),
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobiledivider1", Divider = true }),
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobilevertical", IconClass = "n2-icon-mobile-phone", Title = "Mobile", ClientAction = "$emit('device-preview', { title:'Mobile', w: 480, h: 780 })" }),
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobilehorizontal", IconClass = "n2-icon-mobile-phone tilted90", Title = "Mobile horizontal", ClientAction = "$emit('device-preview', { title: 'Mobile (horizontal)', w: 780, h: 480 })" }),
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobiledivider2", Divider = true }),
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "tabletvertical", IconClass = "n2-icon-tablet", Title = "Tablet", ClientAction = "$emit('device-preview', { title: 'Tablet (horizontal)', w: 780, h: 1024 })" }),
-						new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "tablethorizontal", IconClass = "n2-icon-tablet tilted90", Title = "Tablet horizontal", ClientAction = "$emit('device-preview', { title: 'Tablet (horizontal)', w: 1024, h: 780 })" }),
-					}
+					//Children = new Node<InterfaceMenuItem>[]
+					//{
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "pc", IconClass = "n2-icon-laptop", Title = "PC", ClientAction = "$emit('device-restore', null)" }),
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobiledivider1", Divider = true }),
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobilevertical", IconClass = "n2-icon-mobile-phone", Title = "Mobile", ClientAction = "$emit('device-preview', { title:'Mobile', w: 480, h: 780 })" }),
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobilehorizontal", IconClass = "n2-icon-mobile-phone tilted90", Title = "Mobile horizontal", ClientAction = "$emit('device-preview', { title: 'Mobile (horizontal)', w: 780, h: 480 })" }),
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "mobiledivider2", Divider = true }),
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "tabletvertical", IconClass = "n2-icon-tablet", Title = "Tablet", ClientAction = "$emit('device-preview', { title: 'Tablet (horizontal)', w: 780, h: 1024 })" }),
+					//	new Node<InterfaceMenuItem>(new InterfaceMenuItem { Name = "tablethorizontal", IconClass = "n2-icon-tablet tilted90", Title = "Tablet horizontal", ClientAction = "$emit('device-preview', { title: 'Tablet (horizontal)', w: 1024, h: 780 })" }),
+					//}
+					Children = config.GetContentSection<Configuration.TargetingSection>("targeting", required: false).PreviewSizes.AllElements
+						.Select(te => new Node<InterfaceMenuItem>(new InterfaceMenuItem { Title = te.Title, Name = te.Name, IconClass = te.IconClass, ClientAction = string.Format("$emit('device-preview', {0})", new { te.Title, te.Name, te.IconClass, te.Width, te.Height }.ToJson()) })).ToList()
 				}, insertBeforeSiblingWithName: "previewdivider1");
 		}
 
