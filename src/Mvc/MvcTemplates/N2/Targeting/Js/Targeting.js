a17874ce55b020433a9aa5281a3a069724ba2914
Cristian Libardo <cristianlibardo@hotmail.com>
2013-07-11 22:18:56 +0200

    Previewed device selection

--- src/Mvc/MvcTemplates/N2/Targeting/Js/Targeting.js
+++ src/Mvc/MvcTemplates/N2/Targeting/Js/Targeting.js
@@ -3,7 +3,7 @@
 		targeting: {
 			menu: {
 				fullsize: { text: " Full size" },
-				rotate: { text: " Switch portrait/landscape" }
+				rotate: { text: " Rotate" }
 			},
 			preview: {
 				custom: "Custom",
@@ -13,18 +13,52 @@
 		}
 	});
 
+	function appendFlag(flag) {
+		$scope.Context.Flags.push(flag);
+	};
+	function removeFlag(flag) {
+		var index = $scope.Context.Flags.indexOf(flag);
+		if (index >= 0)
+			$scope.Context.Flags.splice(index, 1);
+	};
+
+
+	function appendPreviewFlag() {
+		removeFlag("PreviewFullscreen");
+		removeFlag("PreviewTilted")
+
+		if ($scope.preview)
+			appendFlag("Preview" + $scope.preview.size.Name);
+		else
+			appendFlag("PreviewFullscreen");
+	}
+
+	function removePreviewFlag() {
+		if (!$scope.preview)
+			return;
+
+		removeFlag("Preview" + $scope.preview.size.Name);
+	}
+
 	$rootScope.$on("contextchanged", function (scope, e) {
 		if ($scope.Context.Flags.indexOf('Management') >= 0)
 			delete $scope.preview;
+		else
+			appendPreviewFlag();
 	});
 
 	function rotate() {
 		var s = $scope.preview.size;
 		var newDimensions = { Height: s.Width, Width: s.Height };
 		angular.extend(s, newDimensions);
+		if ($scope.isFlagged("PreviewTilted"))
+			removeFlag("PreviewTilted")
+		else
+			appendFlag("PreviewTilted");
 	}
 
 	$rootScope.$on("device-preview", function (e, size) {
+		removePreviewFlag();
 		if ($scope.preview && $scope.preview.size && $scope.preview.size.Height == size.Height && $scope.preview.size.Width == size.Width) {
 			rotate();
 			return;
@@ -33,10 +67,12 @@
 			size: size,
 			url: window.frames.preview && window.frames.preview.window.location.toString()
 		};
-		console.log($scope.preview);
+		appendPreviewFlag();
 	});
 	$rootScope.$on("device-close", function (e, size) {
+		removePreviewFlag();
 		delete $scope.preview;
+		appendPreviewFlag();
 	});
 	$rootScope.$on("device-rotate", function (e, size) {
 		if ($scope.preview) {
@@ -44,7 +80,7 @@
 		}
 	});
 	$rootScope.$on("resized", function () {
-		$scope.preview.size.Title = Translate("targeting.preview.custom");// "Custom";
+		$scope.preview.size.Title = Translate("targeting.preview.custom");
 	});
 
 	$scope.frameLoaded = function (e) {
@@ -57,3 +93,10 @@
 		}
 	};
 };
+
+function DevicePreviewMenuCtrl($scope) {
+	$scope.isPreviewing = function () {
+		console.log("isPreviewing", !!$scope.preview);
+		return !!$scope.preview;
+	}
+}
