563ed3144f3fde6166974c30b25b99b222160d01
Cristian Libardo <cristianlibardo@hotmail.com>
2013-08-02 23:08:42 +0200

    Assembly version 2.5.3

--- src/Framework/AssemblyVersion.cs
+++ src/Framework/AssemblyVersion.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 // Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("2.5")]
-[assembly: AssemblyFileVersion("2.5.2.1")]
+[assembly: AssemblyFileVersion("2.5.3")]
