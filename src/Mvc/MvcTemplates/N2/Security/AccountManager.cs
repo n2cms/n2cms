060e05b9b59d2da5c459bf0c38f6233ca14f32df
janpub JH <janpub@gmail.com>
2013-12-01 22:29:37 +0100

    N2 backoffice: configurable account pages
    N2.Security.AccountManager (new)
    Defines account page tokens (default values supports existing membership
    pages). Manager may be later redefined (eg by Aspnet.Identity
    subsystem).
    ManagementItem, InterfaceBuilder now use new tokens instead of hardcoded
    paths to membership pages.
    (ResolveTokens commit is a prerequisite)

---
+++ src/Mvc/MvcTemplates/N2/Security/AccountManager.cs
@@ -0,0 +1,38 @@
+﻿using N2.Engine;
+using N2.Plugin;
+
+namespace N2.Security
+{
+    /// <summary>
+    /// User accounts management resources
+    /// </summary>
+    [Service]
+    public class AccountManager : IAutoStart
+	{
+        
+        public AccountManager()
+		{
+		}
+
+        public virtual void Start()
+        {  
+            N2.Web.Url.SetToken(LogoutPageUrlToken,       "{ManagementUrl}/Login.aspx?logout=true");
+            N2.Web.Url.SetToken(UsersPageUrlToken,        "{ManagementUrl}/Users/Users.aspx");
+            N2.Web.Url.SetToken(UsersEditPageUrlToken,    "{ManagementUrl}/Users/Edit.aspx");
+            N2.Web.Url.SetToken(RolesPageUrlToken ,       "{ManagementUrl}/Roles/Roles.aspx");
+            N2.Web.Url.SetToken(EditPasswordPageUrlToken, "{ManagementUrl}/Myself/EditPassword.aspx");
+            
+        }
+
+        public virtual void Stop()
+        {
+        }
+
+        // Framework\N2\Edit\Api\InterfaceBuilder.cs
+        public const string LogoutPageUrlToken = "{Account.Logout.PageUrl}";
+        public const string UsersPageUrlToken  = "{Account.Users.PageUrl}";
+        public const string UsersEditPageUrlToken = "{Account.Users.Edit.PageUrl}";
+        public const string RolesPageUrlToken  = "{Account.Roles.PageUrl}";
+        public const string EditPasswordPageUrlToken  = "{Account.EditPassword.PageUrl}";
+    }
+}
