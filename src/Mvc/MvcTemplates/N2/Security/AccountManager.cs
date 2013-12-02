f8b341f23360cd27ced39585a0e8b020df1a5005
janpub JH <janpub@gmail.com>
2013-12-02 10:14:59 +0100

    N2 backoffice: continued {Account.*}
    LogOn.cshtml, Top.master - tokens instead of hardcoded urls

--- src/Mvc/MvcTemplates/N2/Security/AccountManager.cs
+++ src/Mvc/MvcTemplates/N2/Security/AccountManager.cs
@@ -9,13 +9,13 @@ namespace N2.Security
     [Service]
     public class AccountManager : IAutoStart
 	{
-        
         public AccountManager()
 		{
 		}
 
         public virtual void Start()
-        {  
+        {
+            N2.Web.Url.SetToken(LoginPageUrlToken,        "{ManagementUrl}/Login.aspx");
             N2.Web.Url.SetToken(LogoutPageUrlToken,       "{ManagementUrl}/Login.aspx?logout=true");
             N2.Web.Url.SetToken(UsersPageUrlToken,        "{ManagementUrl}/Users/Users.aspx");
             N2.Web.Url.SetToken(UsersEditPageUrlToken,    "{ManagementUrl}/Users/Edit.aspx");
@@ -29,10 +29,17 @@ namespace N2.Security
         }
 
         // Framework\N2\Edit\Api\InterfaceBuilder.cs
-        public const string LogoutPageUrlToken = "{Account.Logout.PageUrl}";
-        public const string UsersPageUrlToken  = "{Account.Users.PageUrl}";
-        public const string UsersEditPageUrlToken = "{Account.Users.Edit.PageUrl}";
-        public const string RolesPageUrlToken  = "{Account.Roles.PageUrl}";
+        // Mvc\MvcTemplates\N2\ManagementItem.cs
+        // MvcTemplates\N2\Top.master
+        // Dinamico\Themes\Default\Views\Shared\LayoutPartials\LogOn.cshtml
+        public const string LoginPageUrlToken         = "{Account.Login.PageUrl}";
+        public const string LogoutPageUrlToken        = "{Account.Logout.PageUrl}";
+        public const string UsersPageUrlToken         = "{Account.Users.PageUrl}";
+        public const string UsersEditPageUrlToken     = "{Account.Users.Edit.PageUrl}";
+        public const string RolesPageUrlToken         = "{Account.Roles.PageUrl}";
         public const string EditPasswordPageUrlToken  = "{Account.EditPassword.PageUrl}";
+
+        // TODO: login dialog as part (external providers live in full pages)
+        // Dinamico\Themes\Default\Views\ContentParts\LoginForm.cshtml  (ContentPart)
     }
 }
