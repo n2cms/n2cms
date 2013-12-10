25623b8bbffd5e0223f43352c0406b29ee9beaea
Peter Davis <peter@lithe.net.au>
2013-12-10 21:20:28 +1030

    Enable use of value types as for models.

--- src/Framework/Razor/Web/Mvc/ContentWebViewPageOfT.cs
+++ src/Framework/Razor/Web/Mvc/ContentWebViewPageOfT.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace N2.Web.Mvc
 {
 	/// <remarks>This code is here since it has dependencies on ASP.NET 3.0 which isn't a requirement for N2 in general.</remarks>
-	public abstract class ContentWebViewPage<TModel> : WebViewPage<TModel>, IContentView where TModel : class
+	public abstract class ContentWebViewPage<TModel> : WebViewPage<TModel>, IContentView
 	{
 		private DynamicContentHelper content;
 
