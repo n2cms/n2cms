193ea90617d2039fa5b8b30714608f9f12087f9a
Ben Herila <ben@herila.net>
2013-12-12 13:40:14 -0500

    More whitespace

--- src/Framework/N2/Details/EditableMetaTagAttribute.cs
+++ src/Framework/N2/Details/EditableMetaTagAttribute.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Web;
 using System.Web.UI;
 using System;
@@ -6,33 +6,33 @@ using N2.Edit;
 
 namespace N2.Details
 {
-	/// <summary>
-	/// Specifies a text box which renders as a html meta tag element when rendered on a web page.
-	/// </summary>
-	[AttributeUsage(AttributeTargets.Property)]
-	public class EditableMetaTagAttribute : EditableTextAttribute
-	{
-		public override System.Web.UI.Control AddTo(ContentItem item, string detailName, System.Web.UI.Control container)
-		{
-			using (var tw = new StringWriter())
-			{
-				Write(item, detailName, tw);
-				var lc = new LiteralControl(tw.ToString());
-				return lc;
-			}
-		}
+    /// <summary>
+    /// Specifies a text box which renders as a html meta tag element when rendered on a web page.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EditableMetaTagAttribute : EditableTextAttribute
+    {
+        public override System.Web.UI.Control AddTo(ContentItem item, string detailName, System.Web.UI.Control container)
+        {
+            using (var tw = new StringWriter())
+            {
+                Write(item, detailName, tw);
+                var lc = new LiteralControl(tw.ToString());
+                return lc;
+            }
+        }
 
-		public override void Write(ContentItem item, string propertyName, System.IO.TextWriter writer)
-		{
-			string content = item[propertyName] as string;
-			if (string.IsNullOrEmpty(content))
-				return;
+        public override void Write(ContentItem item, string propertyName, System.IO.TextWriter writer)
+        {
+            string content = item[propertyName] as string;
+            if (string.IsNullOrEmpty(content))
+                return;
 
-			writer.Write("<meta name=\"");
-			writer.Write(propertyName.ToLower());
-			writer.Write("\" content=\"");
-			N2.Context.Current.Resolve<ISafeContentRenderer>().HtmlEncode(content, writer);
-			writer.Write("\" />");			
-		}
-	}
+            writer.Write("<meta name=\"");
+            writer.Write(propertyName.ToLower());
+            writer.Write("\" content=\"");
+            N2.Context.Current.Resolve<ISafeContentRenderer>().HtmlEncode(content, writer);
+            writer.Write("\" />");          
+        }
+    }
 }
--- src/Framework/N2/Edit/SafeContentRenderer.cs
+++ src/Framework/N2/Edit/SafeContentRenderer.cs
@@ -1,58 +1,58 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Web;
 using N2.Configuration;
 using N2.Engine;
 
 namespace N2.Edit {
-	/// <summary>
-	/// Class responsible for making sure the content rendered to the client is not containing
-	/// any illegal content like javascript or XSS codes.
-	/// Securing your ASP.Net application article: http://msdn.microsoft.com/en-us/magazine/hh708755.aspx
-	/// XSS cheat sheet: https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet
-	/// XSS examples: https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
-	/// 
-	/// </summary>
-	[Service(typeof(ISafeContentRenderer))]
-	public class SafeContentRenderer : ISafeContentRenderer
-	{
-		protected readonly EngineSection config;
-		protected bool AllowHtml;
+    /// <summary>
+    /// Class responsible for making sure the content rendered to the client is not containing
+    /// any illegal content like javascript or XSS codes.
+    /// Securing your ASP.Net application article: http://msdn.microsoft.com/en-us/magazine/hh708755.aspx
+    /// XSS cheat sheet: https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet
+    /// XSS examples: https://www.owasp.org/index.php/XSS_Filter_Evasion_Cheat_Sheet
+    /// 
+    /// </summary>
+    [Service(typeof(ISafeContentRenderer))]
+    public class SafeContentRenderer : ISafeContentRenderer
+    {
+        protected readonly EngineSection config;
+        protected bool AllowHtml;
 
-		public SafeContentRenderer(EngineSection config)
-		{
-			this.config = config;
-			AllowHtml = config.DefaultHtmlEscape;
-		}
+        public SafeContentRenderer(EngineSection config)
+        {
+            this.config = config;
+            AllowHtml = config.DefaultHtmlEscape;
+        }
 
-		/// <summary>
-		/// Html encode the supplied value.
-		/// </summary>
-		/// <param name="value">String to html encode</param>
-		/// <returns>The html encoded value</returns>
-		public virtual string HtmlEncode(string value)
-		{
-			return (AllowHtml ? value : HttpUtility.HtmlEncode(value));
-		}
+        /// <summary>
+        /// Html encode the supplied value.
+        /// </summary>
+        /// <param name="value">String to html encode</param>
+        /// <returns>The html encoded value</returns>
+        public virtual string HtmlEncode(string value)
+        {
+            return (AllowHtml ? value : HttpUtility.HtmlEncode(value));
+        }
 
-		public virtual void HtmlEncode(string value, TextWriter writer)
-		{
-			writer.Write(AllowHtml ? value : HttpUtility.HtmlEncode(value));
-		}
+        public virtual void HtmlEncode(string value, TextWriter writer)
+        {
+            writer.Write(AllowHtml ? value : HttpUtility.HtmlEncode(value));
+        }
 
-		/// <summary>
-		/// Strip unsafe html tags from the supplied html string.
-		/// </summary>
-		/// <param name="value">String to strip unsafe html tags from</param>
-		/// <returns>A safe html string</returns>
-		public virtual string GetSafeHtml(string value)
-		{
-			return value;
-		}
+        /// <summary>
+        /// Strip unsafe html tags from the supplied html string.
+        /// </summary>
+        /// <param name="value">String to strip unsafe html tags from</param>
+        /// <returns>A safe html string</returns>
+        public virtual string GetSafeHtml(string value)
+        {
+            return value;
+        }
 
-		public virtual void GetSafeHtml(string value, TextWriter writer)
-		{
-			writer.Write(value);
-		}
-	}
+        public virtual void GetSafeHtml(string value, TextWriter writer)
+        {
+            writer.Write(value);
+        }
+    }
 }
