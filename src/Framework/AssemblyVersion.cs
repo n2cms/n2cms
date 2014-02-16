d7481a4488452339c6e923ead9ca122335852a01
Ben Herila <ben@herila.net>
2014-02-15 22:26:26 -0800

    Fix AssemblyVersion to 3.0.0.1

--- src/Framework/AssemblyVersion.cs
+++ src/Framework/AssemblyVersion.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 // Version information for an assembly consists of the following four values: You can specify all the values or you can default the Revision and Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("3.0")]
-[assembly: AssemblyFileVersion("3.0.0.1-beta")]
+[assembly: AssemblyFileVersion("3.0.0.1")]
