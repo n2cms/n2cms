2a370a77b9e10ad2df6b3e352937606384c5c796
libardo <cristianlibardo@hotmail.com>
2014-04-12 23:09:45 +0200

    Fixed problem using linq queries with xml repository (fixes #504)

---
+++ src/Framework/N2/Persistence/NH/NHQueryProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Linq;
+using N2.Engine;
+
+namespace N2.Persistence.NH
+{
+	[Service(typeof(LinqQueryProvider))]
+	public class NHQueryProvider : LinqQueryProvider
+	{
+		private ISessionProvider session;
+		
+		public NHQueryProvider(ISessionProvider session)
+		{
+			this.session = session;
+		}
+		
+		public override IQueryable<T> Query<T>()
+		{
+			return session.OpenSession.Session.Query<T>();
+		}
+	}
+}
---
+++ src/Framework/N2/Persistence/Xml/XmlQueryProvider.cs
@@ -0,0 +1,24 @@
+﻿using N2.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace N2.Persistence.Xml
+{
+	[Service(typeof(LinqQueryProvider), Configuration = "xml")]
+	public class XmlQueryProvider : LinqQueryProvider
+	{
+		private IEngine engine;
+
+		public XmlQueryProvider(IEngine engine)
+		{
+			this.engine = engine;
+		}
+
+		public override IQueryable<T> Query<T>()
+		{
+			return engine.Resolve<IRepository<T>>().Find().AsQueryable();
+		}
+	}
+}
