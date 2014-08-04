e299c2488e681bad6bcfec9f5b1ec36be7012c0e
janpub JH <janpub@gmail.com>
2014-08-04 11:47:01 +0200

    JH 4 - N2CMS Dinamico on ASP.NET Identity.  To run DinamicoIdentity you should add N2 and Dinamico subfolders. See also: NamespaceDoc (N2.Security.AspNet.Identity)

---
+++ src/Framework/N2.Security.AspNet.Identity/_Doc/ReleaseNotes.cs
@@ -0,0 +1,29 @@
+﻿
+namespace N2.Security.AspNet.Identity
+{
+    /// <summary>
+    /// N2 Aspnet.Identity release notes
+    /// <ul>
+    ///  <li>           - janpub, JH4 oull request: N2 Identity lib and sample app (Visual Studio 2013 Update 2)
+    ///  
+    ///  </li>
+    ///  <li>           - janpub, JH3 pull request: N2 (non-breaking) changes to support Identity
+    ///  
+    ///  </li>
+    ///  <li>2014.07.14 - janpub, JH2 pull request submitted: Basic N2 User subsystem extensions 
+    ///      https://github.com/n2cms/n2cms/pull/544
+    ///  </li>
+    ///  <li>2014.07.12 - janpub, JH1 pull request accepted: Basic code enhancements - Token values may contain tokens
+    ///      https://github.com/n2cms/n2cms/commit/456f5b817063f62c796a536acf6a4417c5a108e1
+    ///  </li>
+    ///  <li>2013.12.05 - janpub, Description published </li>
+    ///  <li>2013.12.05 - janpub, proto app on server http://ae.abaeda.miga.eu </li>
+    ///  <li>2013.12.04 - janpub, N2 Dinamico + N2 Management running, alpha quality (unsupported: N2 installation)</li>
+    ///  <li>2013.12.01 - janpub, N2 Dinamico in MVC5/Aspnet.Identity running, alpha quality (unsupported: N2 management, N2 installation) </li>
+    ///  <li>2013.11.26 - janpub, Start of N2 Aspnet.Identity project <see cref="https://n2cms.codeplex.com/discussions/471621"/> </li>
+    /// </ul>
+    /// </summary>
+    internal class ReleaseNotes
+    {
+    }
+}
