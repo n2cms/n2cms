25205683c4f684148475e2f785fe68b27cf9b26b
Cristian Libardo <cristianlibardo@hotmail.com>
2015-04-12 20:50:48 +0200

    Improved slug generator client code by SntsDev (fixes #631)

--- src/Mvc/MvcTemplates/N2/Resources/Js/plugins/jquery.n2name.js
+++ src/Mvc/MvcTemplates/N2/Resources/Js/plugins/jquery.n2name.js
@@ -1,79 +1,98 @@
 ﻿/**
-* n2name 0.2
+* n2name 0.3
 */
 
-(function($) {
-    function getName(titleid, whitespace, tolower, replacements){
-        var titleBox=document.getElementById(titleid);
-		if (!titleBox) return null;
-	    var name;
+var n2Name = (function ($) {
+	var nameWaitingInt;
+
+	function sanitizeRegex(value) {
+		var n = value.replace(/^\s+|\s+$/g, '')
+			.replace(/[.]+/g, '-')
+			.replace(/[^a-z0-9 -ãàáäâẽèéëêìíïîõòóöôùúüûñç]/g, '')
+			.replace(/\s+/g, whitespace)
+			.replace(/[-]+/g, '-')
+			.replace(/[-]+$/g, '');
+		if (tolower) n = n.toLowerCase();
+
+		var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç";
+		var to = "aaaaaeeeeeiiiiooooouuuunc";
+		for (var i = 0, l = from.length ; i < l ; i++) {
+			n = n.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+		}
+		for (var i in replacements) {
+			n = n.replace(replacements[i].pattern, replacements[i].value);
+		}
+		return n;
+	}
+
+	function getName(titleid, nameid, whitespace, tolower, replacements, checkboxid, callback) {
+		var titleBox = document.getElementById(titleid);
+		if (!titleBox) return;
 
         $.ajax({
-        	type: 'POST',
-        	async: false,
-        	cache: false,
-        	url: '/sluggenerator.n2.ashx',
-        	data: { action: "sluggenerator", title: titleBox.value },
-        	success: function (result) { name = result; },
-        	error: function () {
-        		name = titleBox.value.replace(/[.]+/g, '-')
-        		    .replace(/[%?&/+:<>]/g, '')
-        		    .replace(/\s+/g, whitespace)
-        		    .replace(/[-]+/g, '-')
-        		    .replace(/[-]+$/g, '');
-        		if(tolower) name = name.toLowerCase();
-        		for (var i in replacements){
-        			name = name.replace(replacements[i].pattern, replacements[i].value);
-        		}        		
-        	}
+            type: 'POST',
+            cache: false,
+            url: 'sluggenerator.n2.ashx',
+            data: { action: "sluggenerator", title: titleBox.value },
+            success: function (result) {
+            	callback(result);
+            },
+            error: function () {
+            	callback(sanitizeRegex(titleBox.value));
+            }
         });
+	};
 
-        return name;
-    };
-    
-    function updateName(titleid, nameid, whitespace, tolower, replacements, checkboxid){
-        var name = getName(titleid, whitespace, tolower, replacements);
-        if(checkboxid && document.getElementById(checkboxid).checked)
-	        document.getElementById(nameid).value = name;
-    };
+	function updateName(titleid, nameid, whitespace, tolower, replacements, checkboxid) {
+		if (checkboxid && document.getElementById(checkboxid).checked) {
+			getName(titleid, nameid, whitespace, tolower, replacements, checkboxid, function callback(name) {
+				document.getElementById(nameid).value = name;
+			});
+		}
+	};
 
-    function checkboxHandler(){
-        var checked = $(this).find('input').attr('checked');
-        if (checked)
+	function checkboxHandler() {
+		var checked = $(this).find('input').prop('checked');
+		if (checked)
 			$(this).removeClass('unchecked')
-				.siblings('input').removeClass('disabled').removeAttr('readonly');
-        else
+                .siblings('input').removeClass('disabled').removeAttr('readonly');
+		else
 			$(this).addClass('unchecked')
-				.siblings('input').addClass('disabled').attr('readonly', true);
-    };
-    
-    function toggleChecked($cb){
-        if($cb.attr("checked"))
-            $cb.removeAttr("checked");
-        else
-            $cb.attr("checked", true);
-    };
-    
-    $.fn.n2name = function(options) {
-        var invokeUpdateName = function(){
-	        updateName(options.titleId, options.nameId, options.whitespaceReplacement, options.toLower, options.replacements, options.keepUpdatedBoxId);
-        };
-        if(options.keepUpdatedBoxId){
-            var $ku = $(this).siblings(".keepUpdated");
-            
-            $ku.click(function(e,stop) {
-                toggleChecked($(this).find('input'));
-                checkboxHandler.call(this);
-            });
-            
-            $("#" + options.titleId).keyup(invokeUpdateName).blur(invokeUpdateName);
+                .siblings('input').addClass('disabled').attr('readonly', true);
+	};
 
-            var expected = getName(options.titleId, options.whitespaceReplacement, options.toLower, options.replacements);
-            var actual = $("#" + options.nameId).attr("value");
-            if(!expected || !actual || (expected == actual))
-                $ku.each(checkboxHandler)
-            else
-                $ku.trigger('click');
-        }
-    };
+	var ebbTimeout = null;
+	function ebb(callback, timeout) {
+		ebbTimeout && clearTimeout(ebbTimeout);
+		ebbTimeout = setTimeout(callback, timeout || 500);
+	}
+
+	$.fn.n2name = function (options) {
+		var invokeUpdateName = function () {
+			ebb(function () {
+				updateName(options.titleId, options.nameId, options.whitespaceReplacement, options.toLower, options.replacements, options.keepUpdatedBoxId);
+			});
+			
+		};
+		if (options.keepUpdatedBoxId) {
+			var $ku = $(this).siblings(".keepUpdated");
+
+			$ku.click(function (e, stop) {
+				var $cb = $(this).find('input');
+				$cb.prop("checked", !$cb.prop("checked"));
+				checkboxHandler.call(this);
+				invokeUpdateName();
+			});
+
+			$("#" + options.titleId).keyup(invokeUpdateName);
+
+			getName(options.titleId, options.nameId, options.whitespaceReplacement, options.toLower, options.replacements, options.keepUpdatedBoxId, function callback(expected) {
+				var actual = $("#" + options.nameId).attr("value");
+				if (!expected || !actual || (expected == actual))
+					$ku.each(checkboxHandler)
+				else
+					$ku.trigger('click');
+			});
+		}
+	};
 })(jQuery);;
