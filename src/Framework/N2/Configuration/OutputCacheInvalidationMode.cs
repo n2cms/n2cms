dc23cf6f48842f44713ec69ecb840ece5eb19098
Cristian Libardo <cristianlibardo@hotmail.com>
2015-05-10 23:47:24 +0200

    Fix issue with GetChildren on globalization page (fixes #636)

---
+++ src/Framework/N2/Configuration/OutputCacheInvalidationMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace N2.Configuration
+{
+	public enum OutputCacheInvalidationMode
+	{
+		All,
+		Site,
+		SiteSection,
+		Page,
+		IgnoreChanges
+	}
+}
