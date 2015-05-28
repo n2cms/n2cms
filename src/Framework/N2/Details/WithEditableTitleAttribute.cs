5387e752ffa730ff1cdddfb9c60196316b2f93a9
Cristian Libardo <cristianlibardo@hotmail.com>
2015-05-28 23:02:38 +0200

    Heading level on editable title

--- src/Framework/N2/Details/WithEditableTitleAttribute.cs
+++ src/Framework/N2/Details/WithEditableTitleAttribute.cs
@@ -44,6 +44,8 @@ namespace N2.Details
 
         public string CssClass { get; set; }
 
+		public int HeadingLevel { get; set; }
+
         public override bool UpdateItem(ContentItem item, Control editor)
         {
             var tb = (TextBox)editor;
@@ -79,17 +81,17 @@ namespace N2.Details
             object value = item[propertyName];
             if (value != null)
             {
-                int headingLevel = item.IsPage ? 1 : 2;
-            writer.Write("<h");
-            writer.Write(headingLevel);
-            if(CssClass != null)
-                writer.Write(" class='" + CssClass + "'");
-            writer.Write(">");
-                writer.Write(value);
-            writer.Write("</h");
-            writer.Write(headingLevel);
-            writer.Write(">");
-        }
+                int headingLevel = HeadingLevel > 0 ? HeadingLevel : item.IsPage ? 1 : 2;
+				writer.Write("<h");
+				writer.Write(headingLevel);
+				if(CssClass != null)
+					writer.Write(" class='" + CssClass + "'");
+				writer.Write(">");
+					writer.Write(value);
+				writer.Write("</h");
+				writer.Write(headingLevel);
+				writer.Write(">");
+			}
         }
 
         #endregion
@@ -103,7 +105,7 @@ namespace N2.Details
             {
                 var heading = new Hn
                 {
-                    Level = item.IsPage ? 1 : 2,
+                    Level = HeadingLevel > 0 ? HeadingLevel : item.IsPage ? 1 : 2,
                     Text = value.ToString(),
                     CssClass = CssClass
                 };
