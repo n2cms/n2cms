e120e2bd1208ac8fcc438e7b83642164f1ce61dc
Cristian Libardo <cristianlibardo@hotmail.com>
2015-07-28 11:40:19 +0200

    Fix control panel positioning

--- src/Framework/N2/Web/Mvc/Html/ControlPanelExtensions.cs
+++ src/Framework/N2/Web/Mvc/Html/ControlPanelExtensions.cs
@@ -239,10 +239,10 @@ namespace N2.Web.Mvc.Html
                 if (includePartScripts) resources.PartsJs();
                 if (includePartStyles) { resources.PartsCss(); resources.IconsCss(); }
 
-                if (refreshNavigation)
-                    writer.Write(formatWithRefresh.Replace(settings));
+				if (refreshNavigation)
+					resources.HtmlLiteral(formatWithRefresh.Replace(settings));
                 else
-                    writer.Write(formatWithoutRefresh.Replace(settings));
+					resources.HtmlLiteral(formatWithoutRefresh.Replace(settings));
 
                 if (state.IsFlagSet(ControlPanelState.DragDrop))
                     resources.JavaScript(UI.WebControls.ControlPanel.DragDropScriptInitialization(item), ScriptOptions.DocumentReady);
