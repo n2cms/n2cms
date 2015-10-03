00e6d9ce36aaafd5e001dbcefdd798f2eee6b263
Cristian Libardo <cristianlibardo@hotmail.com>
2015-10-03 14:50:58 +0200

    Css tweaks

--- src/Mvc/MvcTemplates/N2/Statistics/Js/Statistics.js
+++ src/Mvc/MvcTemplates/N2/Statistics/Js/Statistics.js
@@ -7,11 +7,14 @@
 			globalMax *= 10;
 		return globalMax;
 	}
+	$scope.max = 1;
 	$rootScope.$on("contextchanged", function (scope, args) {
+		$scope.loading = true;
 		res.statistics({ n2item: args.CurrentItem.ID }, function (result) {
 			$rootScope.statisticsMaximum = getMaximum(result.Max, $rootScope.statisticsMaximum || 1)
 			$scope.max = $rootScope.statisticsMaximum;
 			$scope.Statistics = result;
+			$scope.loading = false;
 		});
 	});
 };
