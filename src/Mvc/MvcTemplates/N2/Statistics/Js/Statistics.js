030c4882b3235f7b118f6b96c855e7b5acfdf6f2
Cristian Libardo <cristianlibardo@hotmail.com>
2015-10-03 22:47:42 +0200

    Avoid loading statistics on closed info panel

--- src/Mvc/MvcTemplates/N2/Statistics/Js/Statistics.js
+++ src/Mvc/MvcTemplates/N2/Statistics/Js/Statistics.js
@@ -8,13 +8,23 @@
 		return globalMax;
 	}
 	$scope.max = 1;
-	$rootScope.$on("contextchanged", function (scope, args) {
-		$scope.loading = true;
-		res.statistics({ n2item: args.CurrentItem.ID }, function (result) {
+	function loadStatistics() {
+		res.statistics({ n2item: $scope.Context.CurrentItem.ID }, function (result) {
 			$rootScope.statisticsMaximum = getMaximum(result.Max, $rootScope.statisticsMaximum || 1)
 			$scope.max = $rootScope.statisticsMaximum;
 			$scope.Statistics = result;
-			$scope.loading = false;
+			$scope.stale = false;
 		});
+	};
+	$rootScope.$on("contextchanged", function (scope, args) {
+		$scope.stale = true;
+		if ($scope.showInfo) {
+			loadStatistics();
+		}
+	});
+	$scope.$watch("showInfo", function (show) {
+		if (show && $scope.stale) {
+			loadStatistics();
+		}
 	});
 };
