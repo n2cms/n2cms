18be27a94188f9e2dbbdcb1dec3e35a67f062696
Cristian Libardo <cristianlibardo@hotmail.com>
2015-10-06 23:31:24 +0200

    Error message tweak

--- src/Mvc/MvcTemplates/N2/Statistics/StatisticsErrorMessageProvider.cs
+++ src/Mvc/MvcTemplates/N2/Statistics/StatisticsErrorMessageProvider.cs
@@ -12,7 +12,7 @@ namespace N2.Management.Statistics
 		public override IEnumerable<CollaborationMessage> GetMessages(N2.Edit.Collaboration.CollaborationContext context)
 		{
 			if (SqlBucketRepository.AdoExceptionDetected.HasValue)
-				yield return new CollaborationMessage { Title = "Statistics error", Updated = SqlBucketRepository.AdoExceptionDetected.Value, Alert = true, Text = "An error was logged when saving or retrieving statistics. <a href='/n2/installation/upgrade.aspx'>An upgrade might be required to set up statistics. <b class='ico fa fa-external-link'></b></a>" };
+				yield return new CollaborationMessage { Title = "Statistics error", Updated = SqlBucketRepository.AdoExceptionDetected.Value, Alert = false, Text = "An error was logged when saving or retrieving statistics. <a href='/n2/installation/upgrade.aspx'>An upgrade might be required to set up statistics. <b class='ico fa fa-external-link'></b></a>", RequiredPermission = Security.Permission.Administer };
 		}
 	}
 }
