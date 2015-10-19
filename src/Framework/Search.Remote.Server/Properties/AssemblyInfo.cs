316f577b094d59258d93d6d8c8fe1e8797ae5823
Cristian Libardo <cristianlibardo@hotmail.com>
2015-10-19 20:50:20 +0200

    Same shared assembly version on multiple assemblies

--- src/Framework/Search.Remote.Server/Properties/AssemblyInfo.cs
+++ src/Framework/Search.Remote.Server/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("85766ee1-4363-4d27-bcd5-c7e10f73b917")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Mvc/MvcTemplates/N2/Collaboration/Properties/AssemblyInfo.cs
+++ src/Mvc/MvcTemplates/N2/Collaboration/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("71dce4f2-fa54-49c7-8ca8-f2937e692189")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
