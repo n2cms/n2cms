a233ae8dd8188b396b5189b9c5c414f200e75ad2
libardo <cristianlibardo@hotmail.com>
2015-11-29 22:12:03 +0100

    First appearance of ITranslator

---
+++ src/Framework/N2/Engine/Globalization/ITranslator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2.Engine.Globalization
+{
+	/// <summary>
+	/// Provides translated texts for end user user interfaces.
+	/// </summary>
+	public interface ITranslator
+	{
+		string Translate(string key, string fallback);
+	}
+}
