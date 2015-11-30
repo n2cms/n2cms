6a5e54839631f144a714ce88b94d902237352dc6
libardo <cristianlibardo@hotmail.com>
2015-11-30 23:06:04 +0100

    Additional merge changes

--- src/Mvc/MvcTemplates/N2/App/Js/FramedControllers.js
+++
@@ -1,8 +0,0 @@
-﻿var n2framed = angular.module('n2framed', ['n2.directives', 'n2.services'], function () {
-});
-
-n2framed.controller("EditLayoutCtrl", ["$scope", function ($scope) {
-	$scope.toggleSidebar = function () {
-		$scope.sidebarOpen = !$scope.sidebarOpen;
-	}
-}])
