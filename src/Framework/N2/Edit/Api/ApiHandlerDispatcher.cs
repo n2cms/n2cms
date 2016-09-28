efe0cef8460c9760fa3f8b35cbaa7c08a8cbce79
Ben Herila <ben@herila.net>
2016-09-28 18:17:15 +0000

    Use case-insensitive match when finding the handler which matches the request

--- src/Framework/N2/Edit/Api/ApiHandlerDispatcher.cs
+++ src/Framework/N2/Edit/Api/ApiHandlerDispatcher.cs
@@ -1,30 +1,28 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web;
 using N2.Web;
 
 namespace N2.Management.Api
 {
-    public class ApiHandlerDispatcher : IHttpHandler
-    {
-        public bool IsReusable
-        {
-            get { return true; }
-        }
+	public class ApiHandlerDispatcher : IHttpHandler
+	{
+		public bool IsReusable => true;
 
-        public void ProcessRequest(HttpContext context)
-        {
-            var dir = N2.Web.Url.ResolveTokens("{ManagementUrl}/Api/");
-            if (!context.Request.FilePath.StartsWith(dir, StringComparison.InvariantCultureIgnoreCase))
-                return;
+		public void ProcessRequest(HttpContext context)
+		{
+			var dir = Url.ResolveTokens("{ManagementUrl}/Api/");
+			if (!context.Request.FilePath.StartsWith(dir, StringComparison.InvariantCultureIgnoreCase))
+				return;
 
-            var name = context.Request.FilePath.Substring(dir.Length).Replace(".ashx", "Handler");
+			var name = context.Request.FilePath.Substring(dir.Length).Replace(".ashx", "Handler");
 
-            var handler = Context.Current.Container.ResolveAll<IApiHandler>().FirstOrDefault(h => h.GetType().Name == name);
-            if (handler != null)
-                handler.ProcessRequest(context.GetHttpContextBase());
-        }
-    }
+			// Use case insensitive match when finding the handler which matches the request
+			var handler =
+				Context.Current.Container.ResolveAll<IApiHandler>()
+					.FirstOrDefault(h => h.GetType().Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+
+			handler?.ProcessRequest(context.GetHttpContextBase());
+		}
+	}
 }
