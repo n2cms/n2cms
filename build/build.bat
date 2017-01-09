dbba5ca5f6870df4694ea45228bced8ba6871ac4
Ben Herila <ben@herila.net>
2017-01-09 23:59:40 +0000

    Remove dependency on specific VS toolchain (fixes build when VS 2013 is not installed)

--- build/build.bat
+++ build/build.bat
@@ -2,4 +2,4 @@ path "C:\Program Files (x86)\MSBuild\14.0\Bin\";%windir%\Microsoft.NET\Framework
 
 @IF NOT EXIST MSBuild.exe @ECHO COULDN'T FIND MSBUILD (Is .NET 4 installed?)
 
-msbuild n2.proj /maxcpucount /p:VisualStudioVersion=12.0 %*
+msbuild n2.proj /maxcpucount %*
