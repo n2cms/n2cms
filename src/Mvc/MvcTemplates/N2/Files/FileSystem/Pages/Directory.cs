bd1e1cbddcba69d81dedb9d5d2dea228df811cd5
Michael Foti <michael.foti@bobit.com>
2017-07-06 14:18:37 -0700

    EVT-150: changed folder to the open (not filled with black) version

--- src/Mvc/MvcTemplates/N2/Files/FileSystem/Pages/Directory.cs
+++ src/Mvc/MvcTemplates/N2/Files/FileSystem/Pages/Directory.cs
@@ -8,7 +8,7 @@ using N2.Management.Api;
 namespace N2.Edit.FileSystem.Items
 {
     [PageDefinition("Directory",
-        IconClass = "fa fa-folder-close",
+        IconClass = "fa fa-folder-o",
         InstallerVisibility = InstallerHint.NeverRootOrStartPage,
         SortOrder = 2015)]
     [RestrictParents(typeof(AbstractDirectory))]
@@ -63,7 +63,7 @@ namespace N2.Edit.FileSystem.Items
             get
             {
                 if (base.GetFiles().Count > 0)
-                    return "fa fa-folder-open";
+                    return "fa fa-folder-open-o";
                 return base.IconClass;
             }
         }
