8f9c78299129022a86006615979e17555b360c4d
Brian Kim <brian.kim@bobit.com>
2017-07-21 10:58:27 -0700

    Added EditableDropDownWithValidationAttribute. DropDownList that allows addition of child controls => validation control.

---
+++ src/Framework/N2/Web/UI/WebControls/DropDownListWithValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+
+namespace N2.Web.UI.WebControls
+{
+    public class DropDownListWithValidation : System.Web.UI.WebControls.DropDownList
+    {
+        protected override ControlCollection CreateControlCollection()
+        {
+            return new ControlCollection(this);
+        }
+    }
+}
