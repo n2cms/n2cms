8d88c54d4089ff0f7a9d99ecac97a52980c87255
Michael Foti <michael.foti@bobit.com>
2017-07-25 16:34:26 -0700

    changed how post-list URL is constructed for NotExpandible items

--- src/Framework/N2/Edit/NodeAdapter.cs
+++ src/Framework/N2/Edit/NodeAdapter.cs
@@ -283,7 +283,7 @@ namespace N2.Edit
             
             string url = ManagementPaths.GetPreviewUrl(item);
             if (item.NotExpandable)
-                url = string.Format("/post-list{0}",url);
+                url = string.Format("/post-list/{0}/{1}", item.Parent.Name, item.Name);
 
             return String.IsNullOrEmpty(url) ? ManagementPaths.ResolveResourceUrl("{ManagementUrl}/Empty.aspx") : url;
             
