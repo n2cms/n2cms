58e84965a632d453b1d6e7e7947bf98fefbffe3f
Brian Kim <brian.kim@bobit.com>
2017-08-02 11:12:24 -0700

    EditableBooleanAttribute to save value as bool instead of string.

--- src/Framework/N2/Details/EditableBooleanAttribute.cs
+++ src/Framework/N2/Details/EditableBooleanAttribute.cs
@@ -7,6 +7,15 @@ namespace N2.Details
 {
     public class EditableBooleanAttribute : EditableRadioListAttribute
     {
+        public override bool UpdateItem(N2.ContentItem item, Control editor)
+        {
+            var rbl = editor as RadioButtonList;
+            var tf = Convert.ToBoolean(rbl.SelectedValue);
+            item[this.Name] = tf;
+
+            return true;
+        }
+       
         protected override IEnumerable<ListItem> GetListItems(Control container)
         {
             return new ListItem[]
