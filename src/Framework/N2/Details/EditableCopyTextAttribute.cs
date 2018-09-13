7151193b5436f08a4ae45a59f93adb854bdf9763
Brian Kim <brian.kim@bobit.com>
2018-09-13 16:12:42 -0700

    EditableCopyTextAttribute. Used for readonly text click to copy purposes.

---
+++ src/Framework/N2/Details/EditableCopyTextAttribute.cs
@@ -0,0 +1,218 @@
+#region License
+/* Copyright (C) 2007 Cristian Libardo
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+#endregion
+
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using N2.Persistence.Search;
+using System.IO;
+using N2.Web.UI.WebControls;
+using System.Web;
+using System.Text;
+
+namespace N2.Details
+{
+    /// <summary>
+    /// Attribute used to mark properties as editable. This attribute is predefined to use 
+    /// the <see cref="System.Web.UI.WebControls.TextBox"/> web control as editor.</summary>
+    /// <example>
+    /// [N2.Details.EditableText("Heading", 80)]
+    /// public virtual string Heading { get; set; }
+    /// 
+    /// [N2.Details.EditableText("Published", 80)]
+    /// public virtual DateTime PublishedDate { get; set; }
+    /// </example>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EditableCopyTextAttribute : AbstractEditableAttribute, IDisplayable, IWritingDisplayable
+    {
+        private int maxLength = 0;
+        private int columns = 0;
+        private int rows = 0;
+        private TextBoxMode textMode = TextBoxMode.MultiLine;
+        private bool readOnly = true;
+
+        public EditableCopyTextAttribute()
+            : this(null, 50)
+        {
+        }
+
+        /// <summary>Initializes a new instance of the EditableTextBoxAttribute class.</summary>
+        /// <param name="title">The label displayed to editors</param>
+        /// <param name="sortOrder">The order of this editor</param>
+        public EditableCopyTextAttribute(string title, int sortOrder)
+            : base(title, sortOrder)
+        {
+		}
+
+        #region Properties
+
+        /// <summary>Gets or sets columns on the text box.</summary>
+        public int Columns
+        {
+            get { return columns; }
+            set { columns = value; }
+        }
+
+        /// <summary>Gets or sets rows on the text box.</summary>
+        public int Rows
+        {
+            get { return rows; }
+            set { rows = value; }
+        }
+        
+        /// <summary>Gets or sets the max length of the text box.</summary>
+        public int MaxLength
+        {
+            get { return maxLength; }
+            set { maxLength = value; }
+        }
+        
+        #endregion
+
+        #region IDisplayable Members
+
+        Control IDisplayable.AddTo(ContentItem item, string detailName, Control container)
+        {
+            using (var sw = new StringWriter())
+            {
+                Write(item, detailName, sw);
+
+                string text = sw.ToString();
+                if (string.IsNullOrEmpty(text))
+                    return null;
+
+                Literal l = new Literal();
+                l.Text = text;
+                container.Controls.Add(l);
+                return l;
+
+            }
+        }
+
+        #endregion
+
+        public override bool UpdateItem(ContentItem item, Control editor)
+        {
+            TextBox tb = editor as TextBox;
+            string value = tb.Text;
+            if (DefaultValue is string && tb.Text == (string)DefaultValue)
+                value = null;
+            if(!AreEqual(value, item[Name]))
+            {
+                item[Name] = value;
+                return true;
+            }
+            return false;
+        }
+
+        public override void UpdateEditor(ContentItem item, Control editor)
+        {
+            TextBox tb = editor as TextBox;
+            tb.Text = Utility.Convert<string>(item[Name]) ?? DefaultValue as string;
+        }
+
+        /// <summary>Creates a text box editor.</summary>
+        /// <param name="container">The container control the tetx box will be placed in.</param>
+        /// <returns>A text box control.</returns>
+        protected override Control AddEditor(Control container)
+        {
+            TextBox tb = CreateEditor();
+            tb.Placeholder(GetLocalizedText("Placeholder") ?? Placeholder);
+            tb.ID = Name;
+            tb.CssClass += " textEditor";
+            ModifyEditor(tb);
+            container.Controls.Add(tb);
+            
+            var btn = new Button();
+            btn.ID = Name + "_copy";
+            btn.Text = "Copy Text";
+            btn.Attributes["style"] = "display:block;margin-top:5px;";
+            btn.CausesValidation = false;
+            container.Controls.Add(btn);
+
+            var page = (Page)HttpContext.Current.CurrentHandler;
+
+            var scriptStr = new StringBuilder();
+            scriptStr.Append($@"
+<script>
+$(document).ready(function() {{
+    $('#{tb.ClientID}').focus(function() {{
+        $('#{tb.ClientID}').select();
+    }});
+    $('#{btn.ClientID}').click(function(e) {{
+        e.preventDefault();
+        $('#{tb.ClientID}').select();
+        document.execCommand('copy');
+        setTimeout(function() 
+        {{
+            alert('The text is copied to your clipboard!');
+        }}, 500);
+    }});
+}});
+</script>
+            ");
+
+            ScriptManager.RegisterStartupScript(page, typeof(Page), "CopyTextScript", scriptStr.ToString(), false);
+
+            return tb;
+        }
+
+        /// <summary>Instantiates the text box control.</summary>
+        /// <returns>A text box.</returns>
+        protected virtual TextBox CreateEditor()
+        {
+            return new TextBox();
+        }
+
+        protected virtual void ModifyEditor(TextBox tb)
+        {
+            if (MaxLength > 0) tb.MaxLength = MaxLength;
+            if (Columns > 0) tb.Columns = Columns;
+            if (Rows > 0) tb.Rows = Rows;
+            if (readOnly == true) { tb.ReadOnly = true; tb.CssClass = "copyTextEditorReadOnly"; }  
+            tb.TextMode = textMode;
+        }
+
+        #region IWritingDisplayable Members
+
+        public virtual void Write(ContentItem item, string propertyName, System.IO.TextWriter writer)
+        {
+            writer.Write(item[propertyName]);
+        }
+
+        #endregion
+
+        #region IIndexableProperty Members
+
+        public bool IsIndexable { get; set; }
+
+        public virtual string GetIndexableText(ContentItem item)
+        {
+            object value = item[Name];
+            if (value == null)
+                return null;
+
+            return value.ToString();
+        }
+
+        #endregion
+    }
+    
+}
